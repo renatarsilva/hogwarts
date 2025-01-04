d74c8a18ff4c413940b6d2b4f917901e004e12a0
Renata <renata.rodriguesdasilva@outlook.com>
2025-01-04 15:17:04 -0300

    feat: initialize project

---
+++ src/components/design/Benefits.jsx
@@ -0,0 +1,5 @@
+export const GradientLight = () => {
+  return (
+    <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#28153e] to-[#28206C]/0 to-75% pointer-events-none" />
+  );
+};
